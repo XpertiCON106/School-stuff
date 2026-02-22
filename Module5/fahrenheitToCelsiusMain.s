cbacbd13c4dd665d86a3f4deb528206e77455225
xperticon106 <tahalil106@gmail.com>
2026-02-22 11:16:39 -0500

    f to c

---
+++ Module5/fahrenheitToCelsiusMain.s
@@ -0,0 +1,50 @@
+// fahrenheitToCelsiusMain.s
+// Author: Tahalil I Morsilin
+// Date: 02/12/2026
+
+.text
+.global main
+main:
+    // save address
+    SUB sp, sp, #4
+    STR lr, [sp, #0]
+
+    // prompt
+    LDR r0, =prompt
+    BL printf
+
+    // read input
+    LDR r0, =inputFormat
+    LDR r1, =userInput
+    BL scanf
+
+    // load value
+    LDR r0, =userInput
+    LDR r0, [r0]
+
+    // Calculate C = (F - 32) * 5 / 9
+    // subtract 32
+    SUB r0, r0, #32
+
+    // multiply by 5
+    MOV r1, #5
+    MUL r0, r0, r1
+
+    // divide by 9
+    MOV r1, #9
+    SDIV r0, r0, r1
+
+    // print and return
+    MOV r1, r0
+    LDR r0, =outputMessage
+    BL printf
+
+    LDR lr, [sp, #0]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+.data
+    prompt: .asciz "Enter temperature in Fahrenheit: "
+    inputFormat: .asciz "%d"
+    outputMessage: .asciz "Temperature in Celsius: %d\n"
+    userInput: .word 0
